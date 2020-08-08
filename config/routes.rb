2ef849a01e01a953236e9bca912f6be8b9a4741f
romainbazeler <romain.bazeler@hotmail.com>
2020-08-08 16:19:57 -0400

    add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  get "tasks/new", to: "tasks#new" # The `new` route needs to be *before* `show` route.
   get "tasks/:id", to: "tasks#show", as: :task
 end
