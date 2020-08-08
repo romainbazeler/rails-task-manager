0932070844e47ee23a6141ef17c3f7018be06515
romainbazeler <romain.bazeler@hotmail.com>
2020-08-08 15:34:10 -0400

    add link to show index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
 class TasksController < ApplicationController
+	# List(read) all tasks
 	def index
 		@tasks = Task.all
+	end
+
+	# find(red one) tasks
+	def show
+		@task = Task.find(params[:id])
 	end
 end
 
